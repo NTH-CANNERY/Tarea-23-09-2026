e577a1929fb4bcd0d6221d5fa1de335337b0274d
NTH-CANNERY <matiascardonacanas5@gmail.com>
2026-09-23 15:45:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from vistas import router as vistas_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Al arrancar la aplicación se crea el pool de conexiones a PostgreSQL.
-    await db.connect("postgresql://neondb_owner:npg_qgvYbXeI78EM@ep-patient-math-apihp7g4-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+    await db.connect("")
     yield
     # Al apagar la aplicación, el pool se cierra para liberar las conexiones.
     await db.close()
