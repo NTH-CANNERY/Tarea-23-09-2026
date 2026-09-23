b9f975684cceb0b5b532ea562dcb60f0899b53b2
NTH-CANNERY <matiascardonacanas5@gmail.com>
2026-09-23 15:46:20 -0500

    Update database connection to use environment variable

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from vistas import router as vistas_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Al arrancar la aplicación se crea el pool de conexiones a PostgreSQL.
-    await db.connect("")
+    await db.connect(os.environ["DATABASE_URL"])
     yield
     # Al apagar la aplicación, el pool se cierra para liberar las conexiones.
     await db.close()
